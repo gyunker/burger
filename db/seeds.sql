c61960bcacecd2e7f75642eb201502263f717ea4
Grant Yunker <grantyunker@gmail.com>
2018-08-25 22:24:14 -0700

    Burger app

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger_db.burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burger_db.burgers (burger_name) VALUES ('Double Cheese Burger');
+INSERT INTO burger_db.burgers (burger_name) VALUES ('Impossible Burger');
+INSERT INTO burger_db.burgers (burger_name) VALUES ('Turkey Burger');
